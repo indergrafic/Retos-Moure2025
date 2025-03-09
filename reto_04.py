48de7683b09359f8904861aa62744f1c8f30fae1
indergrafic <indergrafic@yahoo.es>
2025-03-09 16:30:41 +0100

    Terminar reto 4

--- reto_04.py
+++ reto_04.py
@@ -8,13 +8,28 @@ def palindro():
     else:
         print("No es un palindromo.")
 
+def anagrama():
+    ''' -sorted- ordena las palabras e indica si son iguales.'''
+    print(f"{fras1} es anagrama de {fras2}: {sorted(fras1) == sorted(fras2)}")
+
 def isograma():
-    ver_fras1 = {}
-    ver_fras2 = {}
-    ver_fras1.split()
-    print(ver_fras1)
+    ''' Medimos la longitud de una palabra y el set de la otra 
+    para comprobar si tienen el mismo numero de letras'''
+    if len(fras1) == len(set(fras1)):
+        print(f"La longitud de {fras1} es: {len(fras1)}\n\
+y el set de {fras2} es: {len(set(fras2))}\n\
+por lo tanto es un isogramas.")
+    else:
+        print(f"La palabra {fras1} no es un Isogramas.")
+    if len(fras2) == len(set(fras2)):
+        print(f"La longitud de {fras2} es: {len(fras2)}\n\
+y el set de {fras2} es: {len(set(fras2))}\n\
+por lo tanto es un isogramas.")
+    else:
+        print(f"La palabra {fras2} no es un Isogramas.")
     
 
             
 palindro()
+anagrama()
 isograma()
