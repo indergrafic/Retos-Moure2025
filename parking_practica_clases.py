644f497536a5a4b55b11eef210d399d3f19855c9
indergrafic <indergrafic@yahoo.es>
2025-03-30 20:43:38 +0200

    Comprovando errores

--- parking_practica_clases.py
+++ parking_practica_clases.py
@@ -6,12 +6,13 @@ class Parking:
         self.num_plaza = ['A1', 'A2', 'A3', 'A4', 'A5', 'A7', 'A8']
 
     def entrada(self):
-        if len(self.matri_plaza) > 0:
+        if len(self.num_plaza) > 0:
             plaza = self.num_plaza.pop(0)
             self.matri_plaza[self.matricula] = plaza
             print(f"Su plaza de aparcamiento es {plaza}")
             print("Ya puede pasar.")
             print(f"{self.matri_plaza}")
+            print(f"{self.num_plaza}")
 
     def salida(self, matricula):
         print("Gracias por hacer uso de nuestro parking.")
@@ -28,6 +29,8 @@ class Parking:
         for placa, zona in listado.items():
             print(f"El conche con matricula {placa}, se encuentra aparcado en {zona}")
 
-coche1 = Parking("Seat", "1234FCG")
-coche1.entrada()
-coche1.registro_plazas()
+auto1 = Parking("Toyota", "1234GGH")
+auto1.entrada()
+auto2 = Parking("Ford", "3568UEC")
+auto2.entrada()
+auto2.registro_plazas()
